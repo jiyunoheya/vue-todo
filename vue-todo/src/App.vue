b9f4acdf92618ffd46217e01d1f03a6be06829ec
munjihyun <jhmun364068@gmail.com>
2019-10-22 17:18:25 +0900

    setting

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <todo-header></todo-header>
+    <todo-input></todo-input>
+    <todo-list></todo-list>
+    <todo-footer></todo-footer>
+  </div>
+</template>
+
+<script>
+// var my_com ={...} 하고 정의할 때의 "컴포넌트 내용"
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+
+export default {
+  components:{
+    'TodoHeader' : TodoHeader, 
+    'TodoInput' :TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' :TodoFooter,
+  }
+}
+</script>
+
+<style>
+
+</style>
